4f595c2d85335a1e8838f0dc3aedfeebc6f69429
文福明 <wenfm@mingyuanyun.com>
2017-10-28 10:16:02 +0800

    更新socket提交

--- index.js
+++ index.js
@@ -1,94 +1,105 @@
-/*服务器端程序*/
-
-//创建HTTP服务器
+var log4js = require("log4js");  
+var log4js_config = require("./log4js.json");  //加载配置文件
+log4js.configure(log4js_config); 
+var LogFile = log4js.getLogger('log_date');
+LogFile.info('---the server is running---');
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({    
+    extended: true
+}));
+var server = app.listen(8217);//监听3000端口
 var io = require('socket.io').listen(server);
-console.log('the server is running');
-app.use(express.static(__dirname + '/app'));
-
-//Chatroom
-var numUsers = 0;
-var unames = {};
-var usockets = {};
-
-io.on("connection", function(socket) {
-    var addedUser = false;
-    var socketId = '';
-
-    //sysInfo data数据结构(根据时间不同，可以选择不同属性值)
-    //msg: 需要显示的系统提示内容
-    //numUsers: 当前聊天室的用户数 
-    //username: 系统消息关联的用户名
-    socket.on('check username', function(data) {
-        console.log(data.userId);
-        var userId = data.userId;
-        var username = data.clientName;
-        if (addedUser) return;
-        if (username in unames) {
-            socket.emit('username exists', {
-                msg: '昵称已存在,请更换昵称'
-            });
+console.log('the server is running;the port is 3000');
+//LogFile.info('the server is running');  
+var usockets = {};//保存客户端socket对象
+io.on('connection',function(socket){
+    //连接服务器事件
+    socket.on('link service',function(data){
+        console.log('the client user_id is '+data.user_id);
+        if(typeof data.user_id == 'undefined'){
+            return;//user_id是必须字段，为空直接返回
+        }
+        var user_id = data.user_id;
+        usockets[user_id] = socket;
+        if(typeof usockets[user_id] != 'undefined'){
+            console.log('save client success');
+        }
+        //发射事件告诉客户端已经连接成功
+        usockets[user_id].emit('link status',{code:1000,message:'连接成功'});
+    });
+    //新消息事件
+    socket.on('new message',function(data){
+        console.log('the callback user_id is '+data.user_id);
+        if(typeof data.user_id == 'undefined'){
             return;
         }
-        socketId = username;
-        unames[username] = username;
-        usockets[userId] = socket;
-        ++numUsers;
-        addedUser = true;
-
-        socket.emit('login success', '欢迎来到imOline聊天室');
-
-        socket.broadcast.emit('user joined', socketId);
-
-        io.sockets.emit('add userList',{
-            numUsers: numUsers,
-            unames: unames    
+        if(typeof usockets[data.user_id] == 'undefined'){
+            return;
+        }
+        var user_id = data.user_id;
+        //客户端广播消息
+        usockets[user_id].emit('new message',data);
+    });
+    //离开事件
+    socket.on('disconnect',function(data){
+        //用户离开，销毁连接
+        if(typeof data.user_id != 'undefined'){
+            var user_id = data.user_id;
+            delete usockets[user_id];
+        }
+    });
+});
+var client = require('socket.io-client');
+var socket_client = client.connect('http://127.0.0.1:3000');
+app.post('/',function(req,res){
+    if(req.body.user_id && req.body.message){
+        var user_id = req.body.user_id;
+        var message = req.body.message;
+        var data = {};
+        data['message'] = message;
+        data['user_id'] = user_id;
+        socket_client.emit('new message',data);
+        res.send(user_id);
+    } else {
+        //兼容非 Content-Type=application/json 情况
+        var body = '', jsonStr;
+        req.on('data', function (chunk) {
+            body += chunk; //读取参数流转化为字符串
         });
-    });
-
-    socket.on('disconnect', function() {
-        if (addedUser) {
-            --numUsers;
-            addedUser = false;
-            delete unames[socketId];
-            delete usockets[socketId];
-            socket.broadcast.emit('user left', socketId);
-            socket.broadcast.emit('remove userList', {
-                numUsers: numUsers,
-                username: socketId    
-            });
+        req.on('end', function () {
+            //读取参数流结束后将转化的body字符串解析成 JSON 格式
+            try {
+                jsonStr = JSON.parse(body);
+            } catch (err) {
+                jsonStr = null;
+            }
+            if(jsonStr){
+                var user_id = jsonStr.user_id;
+                var message = jsonStr.message;
+                if(user_id){
+                    var data = {};
+                    data['message'] = message;
+                    data['user_id'] = user_id;
+                    socket_client.emit('new message',data);
+                    res.send(user_id);
+                }
+            }
+        });
+    }
+    res.send('error');
+});
+app.get('/',function(req,res){
+    var user_id = req.query.user_id;
+    var message = req.query.message;
+    if(user_id){
+        var data = {};
+        if(message){
+            data['message'] = message;
         }
-    });
-
-    socket.on('typing', function() {
-        socket.broadcast.emit('typing', socketId);
-    });
-
-    socket.on('stop typing', function() {
-        socket.broadcast.emit('stop typing', socketId);
-    });
-
-    //聊天内容数据结构
-    //method: 消息类型，public/private/system
-    //from: 消息来源
-    //to: 消息目的
-    //msg: 消息内容
-    socket.on('new message', function(data) {
-        //console.log('the to is :'.data.to);
-        if (!data.to) {
-            io.sockets.emit('new message', data);
-        } else if (data.to) {
-            usockets[data.to].emit('new message', data);
-            //usockets[data.from].emit('new message', data);
-        } else {
-            usockets[data.from].emit('new message', {
-                mode: 'system',
-                from: '系统',
-                to: data.from,
-                msg: '私聊对象已离开!'
-            });
-        }
-    });
+        data['user_id'] = user_id;
+        socket_client.emit('new message',data);
+        res.send(user_id);
+    }
 });
